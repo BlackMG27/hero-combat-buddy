ac3c2c283f3ccefcba890244fde633aba3ad37c3
BlackMG27 <oike279@gmail.com>
2019-04-04 10:58:41 -0500

    got the html and the htmlRoutes to work

---
+++ server.js
@@ -0,0 +1,26 @@
+//set up the variables 
+var express = require('express');
+var path = require('path');
+var mysql = require('mysql');
+//create the app instance
+var app = express();
+//set up the port 
+var PORT = process.env.PORT || 8000;
+
+//---------------MUST USE OR ELSE DATA WILL BE UNDEFINED -----------------//
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+
+//----------ROUTES-------------
+require('./app/routes/HTMLRoutes')(app);
+//require('./app/routes/apiRoutes')(app);
+
+
+//----------LISTEN TO SERVER----------------
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
